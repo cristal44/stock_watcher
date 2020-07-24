b878fe3c7a45481d8c84b0c988ecea1c0f88d583
hongli <hxue44@gmail.com>
2020-07-24 17:43:53 -0500

    Adding CompetitorAdapter layout to OverviewFragment

---
+++ app/src/main/java/com/example/mywatchlist/CompetitorAdapter.java
@@ -0,0 +1,59 @@
+package com.example.mywatchlist;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class CompetitorAdapter extends RecyclerView.Adapter<CompetitorAdapter.MyViewHolder> {
+    List<Stock> competitors;
+
+
+    public CompetitorAdapter(List<Stock> competitors) {
+        this.competitors = competitors;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.competitors_layout, parent, false);
+        return new CompetitorAdapter.MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Stock stock = competitors.get(position);
+
+        holder.competitorSymbol.setText(stock.getSymbol());
+        holder.competitorCompanyName.setText(stock.getCompanyName());
+        holder.competitorPE.setText(stock.getPe());
+        holder.competitorChange.setText(stock.getChange());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return competitors.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView competitorSymbol;
+        TextView competitorCompanyName;
+        TextView competitorPE;
+        TextView competitorChange;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            competitorSymbol = itemView.findViewById(R.id.competitorSymbol);
+            competitorCompanyName = itemView.findViewById(R.id.competitorName);
+            competitorPE = itemView.findViewById(R.id.competitorPE);
+            competitorChange = itemView.findViewById(R.id.competitorChange);
+        }
+    }
+
+}
