ed1063e4cc0ba4440b9f3b72fcdd911b95919be2
hongli <hxue44@gmail.com>
2020-08-20 21:02:52 -0500

    Refactor code

--- app/src/main/java/com/example/mywatchlist/MyColor.java
+++ app/src/main/java/com/example/mywatchlist/MyColor.java
@@ -2,13 +2,10 @@ package com.example.mywatchlist;
 
 import android.graphics.Color;
 
-import static android.graphics.Color.rgb;
-
 public class MyColor {
-    public final static int GREEN = rgb(15,157,88);
-    public final static int RED = rgb(219,68,55);
-    public final static int GREY = rgb(171,171,171);
-
-    public final static  int ALPHBLUE =Color.argb(50, 34, 87, 245);
+    public final static int GREEN = Color.rgb(15,157,88);
+    public final static int RED = Color.rgb(219,68,55);
+    public final static int GREY = Color.rgb(171,171,171);
     public final static  int BLUE = Color.rgb(34, 87, 245);
+    public final static  int ALPHBLUE =Color.argb(50, 34, 87, 245);
 }
